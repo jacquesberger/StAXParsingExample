2b53240f3985130c4c525347ff903747651313f4
Jacques Berger <jacquesberger@hotmail.com>
2012-04-22 21:56:34 -0400

    Renommer le package du projet.

--- src/org/jberger/staxparsingexample/stax/ZooDocumentReader.java
+++ src/org/jberger/staxparsingexample/stax/ZooDocumentReader.java
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
  */
-package staxexample;
+package org.jberger.staxparsingexample.stax;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
